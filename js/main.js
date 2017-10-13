ca1addd7ce2a48ce49857408a66798a3aa666600
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-10-12 21:32:41 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,24 +2,23 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push("cardOne");
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push("cardTwo");
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	//Checks to see if two cards have been played
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[2]) {
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+}
+
+var flipCard = function(cardId) {
+		checkForMatch();
+		console.log("User flipped " + cards[cardId]);
+	}
+
+flipCard(0);
+flipCard(2);
+
+cardsInPlay.push(cards[0,2]);
