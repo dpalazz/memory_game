859c515cffc95e2e82a2360f305e55f2698a8392
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-10-16 13:18:40 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var cards = [
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamnds.png"
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: "king",
@@ -23,8 +23,8 @@ var cards = [
 
 var cardsInPlay = []; 
 
-var checkForMatch = function() {	
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
 	}
 	else {
@@ -33,24 +33,24 @@ var checkForMatch = function() {
 };
 
 var flipCard = function() {
-		var cardId = this.getAttribute('data-id');
-		console.log('User flipped ' + cards[cardId].rank);
-		cardsInPlay.push(cards[cardId].rank);
-		this.setAttribute('src', cards[cardId].cardImage);
-		if (cardsInPlay.length === 2) {
-			checkForMatch();
-		}
+	var cardId = this.getAttribute('data-id');
+	console.log('User flipped ' + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
 
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
-	var cardElement = document.createElement('img');
-	cardElement.setAttribute('src', 'images/back.png');
-	cardElement.setAttribute('data-id', i);
-	cardElement.addEventListener('click', flipCard);
-	document.getElementById('game-board').appendChild(cardElement);
-}
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 };
 
 createBoard();
